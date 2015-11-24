a70870c39e9946b5a401918c9a6cf293fb2eb186
tanviborkar <borkar.tanvi@yahoo.com>
2015-11-24 18:05:37 -0500

    committing new junit folder

---
+++ src/main/resources/com/hibernate/test/junitTest/RideDAOImplTest.java
@@ -0,0 +1,68 @@
+package com.hibernate.test.junitTest;
+
+import static org.junit.Assert.*;
+import java.util.Date;
+import org.junit.Test;
+import com.hibernate.test.DAO.RideDAOImpl;
+import com.hibernate.test.api.RideDAOInterface;
+import com.hibernate.test.pojo.Ride;
+
+public class RideDAOImplTest {
+
+	@Test
+	public void testCreateRide() {
+		RideDAOInterface rideInterface = RideDAOImpl.getRideDAOImpl();
+		Ride newRide = new Ride();
+		newRide.setStartPoint("New Brunswick");
+		newRide.setDestination("New York");
+		newRide.setStartTime(new Date());
+		newRide.setMaxNoOfPassengers(5);
+		newRide.setPickupOtherThanStartProvided(true);
+		newRide.setPricePerUser(10);
+		newRide.setComments("passed");
+		newRide.setPriceNegotiable(false);
+		newRide.setRidePostedOn(new Date());
+		
+		//User rideOwner = new User()
+		rideInterface.createRide(newRide);
+	}
+
+	/*@Test
+	public void testEditRide() {
+		RideDAOInterface rideInterface = RideDAOImpl.getRideDAOImpl();
+		Ride newRide = rideInterface.fetchRide(1);
+		assertNotNull(newRide);
+		
+		newRide.setStartPoint("Seacaucus");
+		newRide.setPickupOtherThanStartProvided(false);
+		newRide.setPricePerUser(50);
+				
+		//User rideOwner = new User()
+		rideInterface.editRide(newRide);
+	}
+
+	@Test
+	public void testDeleteRide() {
+		RideDAOInterface rideInterface = RideDAOImpl.getRideDAOImpl();
+		Ride newRide = rideInterface.fetchRide(1);
+		assertNotNull(newRide);
+			
+		//User rideOwner = new User()
+		rideInterface.deleteRide(newRide);;
+	}
+
+	@Test
+	public void testFetchRide() {
+		RideDAOInterface rideInterface = RideDAOImpl.getRideDAOImpl();
+		
+		//User rideOwner = new User()
+		Ride ride = rideInterface.fetchRide(1);
+		if(ride != null){
+			System.out.println(ride);
+		}
+		else{
+			System.out.println("Object not found");
+		}
+		assertNotNull(ride);
+	}*/
+}
