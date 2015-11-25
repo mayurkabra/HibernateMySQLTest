0da9d71b65be1591177ea854006367eb9e21a420
tanviborkar <borkar.tanvi@yahoo.com>
2015-11-25 05:01:39 -0500

    hkjhkhk

--- src/main/resources/com/hibernate/test/util/HibernateUtil.java
+++ src/main/resources/com/hibernate/test/util/HibernateUtil.java
@@ -6,6 +6,7 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 import com.hibernate.test.pojo.Request;
+import com.hibernate.test.pojo.RequestRideMapping;
 import com.hibernate.test.pojo.Ride;
 import com.hibernate.test.pojo.Student;  
 import com.hibernate.test.pojo.User;  
@@ -22,6 +23,7 @@ public class HibernateUtil {
 		configuration.addAnnotatedClass(User.class);
 		configuration.addAnnotatedClass(Ride.class);
 		configuration.addAnnotatedClass(Request.class);
+		configuration.addAnnotatedClass(RequestRideMapping.class);
 		
 		// If you miss the below line then it will complaing about a missing dialect setting
 		serviceRegistryBuilder.applySettings(configuration.getProperties());
