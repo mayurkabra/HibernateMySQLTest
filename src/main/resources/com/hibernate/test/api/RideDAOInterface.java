28579d697619bd7e9944c64e08969ea6d14f35f7
tanviborkar <borkar.tanvi@yahoo.com>
2015-11-25 04:30:24 -0500

    committing changes in the ride interface

--- src/main/resources/com/hibernate/test/api/RideDAOInterface.java
+++ src/main/resources/com/hibernate/test/api/RideDAOInterface.java
@@ -1,11 +1,14 @@
 package com.hibernate.test.api;
 
+import java.util.List;
+
 import com.hibernate.test.pojo.Ride;
 
 public interface RideDAOInterface {
 
 	public void createRide(Ride newRide);
 	public void editRide(Ride updatedRide);
-	public void deleteRide(Ride rideToDelete);
+	public void deleteRide(long rideId);
 	public Ride fetchRide(long rideId);
+	public List<Ride> fetchUserRides(long userId);
 }
